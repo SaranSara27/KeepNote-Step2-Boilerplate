4d83ece6fe446a260a1a5b47f48fc9f6e9c5796d
ubuntu <ubuntu@ip-172-31-95-151.ec2.internal>
2020-05-05 12:48:58 +0000

    solution

--- src/main/java/com/stackroute/keepnote/config/ApplicationContextConfig.java
+++ src/main/java/com/stackroute/keepnote/config/ApplicationContextConfig.java
@@ -1,5 +1,18 @@
 package com.stackroute.keepnote.config;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
 /*This class will contain the application-context for the application. 
  * Define the following annotations:
  * @Configuration - Annotating a class with the @Configuration indicates that the 
@@ -8,7 +21,8 @@ package com.stackroute.keepnote.config;
  * @EnableTransactionManagement - Enables Spring's annotation-driven transaction management capability.
  *                  
  * */
-
+@Configuration
+@EnableTransactionManagement
 public class ApplicationContextConfig {
 
 	/*
@@ -17,18 +31,45 @@ public class ApplicationContextConfig {
 	 * name 2. Database URL 3. UserName 4. Password
 	 */
 
-/*
-        Use this configuration while submitting solution in hobbes.
-		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://" + System.getenv("MYSQL_HOST") + ":3306/" + System.getenv("MYSQL_DATABASE")
-				+"?verifyServerCertificate=false&useSSL=false&requireSSL=false");
-		dataSource.setUsername(System.getenv("MYSQL_USER"));
-		dataSource.setPassword(System.getenv("MYSQL_PASSWORD")); */
+	@Bean
+	@Autowired
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		
+        //Use this configuration while submitting solution in hobbes.
+		
+		 dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		 dataSource.setUrl("jdbc:mysql://" + System.getenv("MYSQL_HOST") + ":3306/" +
+		 System.getenv("MYSQL_DATABASE")
+		 +"?verifyServerCertificate=false&useSSL=false&requireSSL=false");
+		 dataSource.setUsername(System.getenv("MYSQL_USER"));
+		 dataSource.setPassword(System.getenv("MYSQL_PASSWORD"));
+		 
+		return dataSource; 
+	}
 
 	/*
 	 * Define the bean for SessionFactory. Hibernate SessionFactory is the factory
 	 * class through which we get sessions and perform database operations.
 	 */
+	@Bean
+	@Autowired
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource());
+		sessionFactory.setPackagesToScan(new String[] { "com.stackroute.keepnote" });
+		sessionFactory.setHibernateProperties(hibernateProperties());
+		return sessionFactory;
+	}
+	
+	private Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		properties.put("hibernate.show_sql", "true");
+		properties.put("hibernate.format_sql", "true");
+		properties.put("hibernate.hbm2ddl.auto", "create");
+		return properties;
+	}
 
 	/*
 	 * Define the bean for Transaction Manager. HibernateTransactionManager handles
@@ -38,4 +79,11 @@ public class ApplicationContextConfig {
 	 * JDBC too. HibernateTransactionManager allows bulk update and bulk insert and
 	 * ensures data integrity.
 	 */
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory s) {
+		HibernateTransactionManager txManager = new HibernateTransactionManager();
+		txManager.setSessionFactory(s);
+		return txManager;
+	}
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,15 +1,91 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8" isELIgnored="false"%>
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 
+<html>
 <head>
-<title>Keep-Board</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>KeepNote</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
 </head>
-
 <body>
-	<!-- Create a form which will have text boxes for Note title, content and status along with a Add 
-		 button. Handle errors like empty fields.  (Use dropdown-list for NoteStatus) -->
+	<div class="page-header">
+		<h1 style="color: darkred; background-color: rgb(202, 238, 135)">Keep
+			Notes</h1>
+	</div>
+	<c:if test="${not empty errorMessage}">
+	   <p style="color:red">Error</p>: ${errorMessage}
+	</c:if>
+	<form:form method="POST" action="add" modelAttribute="note">
+		<div class="container">
+			<table>
+				<tr>
+					<td><form:label path="noteId">Note Id</form:label></td>
+					<td><form:input path="noteId" /></td>
+				</tr>
+				<tr>
+					<td><form:label path="noteTitle">Note Title</form:label></td>
+					<td><form:input path="noteTitle" /></td>
+				</tr>
+				<tr>
+					<td><form:label path="noteContent">Note Content</form:label></td>
+					<td><form:input path="noteContent" /></td>
+				</tr>
+				<tr>
+					<td><form:label path="noteStatus">Status</form:label></td>
+					<td><form:input path="noteStatus" /></td>
+				</tr>
 
-	<!-- display all existing notes in a tabular structure with Title,Content,Status, Created Date and Action -->
+				<tr>
+					<td><button class="btn btn-success my-2 my-sm-0" type="submit">Add Note</button></td>
+				</tr>
+			</table>
+		</div>
+	</form:form>
+
+	<hr />
+	<h2 style="color: blue;">List of Notes</h2>
+	<table class="table table-bordered">
+		<thead style="font-weight: bold">
+			<tr class="danger">
+				<th>Id</th>
+				<th>Title</th>
+				<th>Content</th>
+				<th>Status</th>
+				<th>createdOn</th>
+				<th>Action</th>
+			</tr>
+		</thead>
+		<c:forEach var="notee" items="${notes}">
+			<tr class="success">
+				<td>${notee.noteId}</td>
+				<td>${notee.noteTitle}</td>
+				<td>${notee.noteContent}</td>
+				<td>${notee.noteStatus}</td>
+				<td>${notee.createdAt}</td>
+				<td><form action="update" method="post" >
+						<input type="hidden" id="noteId" name="noteId"
+							value="${notee.noteId}" />
+						<button class="btn btn-primary my-2 my-sm-0" type="submit">Update</button>
+					</form>
+					<form action="delete" method="post">
+						<input type="hidden" id="noteId" name="noteId"
+							value="${notee.noteId}" />
+						<button class="btn btn-warning my-2 my-sm-0" type="submit">Delete</button>
+					</form></td>
+			</tr>
+		</c:forEach>
+	</table>
+
 </body>
 
 </html>
